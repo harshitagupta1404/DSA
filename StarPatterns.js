0bcebe6df841422ad689923af2b373b92b1e1475
harshitagupta1404 <33868012+harshitagupta1404@users.noreply.github.com>
2025-06-14 19:14:18 +0530

    Create StarPatterns.js

---
+++ StarPatterns.js
@@ -0,0 +1,43 @@
+// ***
+// ***
+// ***   n=3
+function star(n) {
+    for (let i = 0; i < n; i++){
+        let row = "";
+        for (let j = 0; j < n; j++){
+            row += '* ';
+        }
+        console.log(row);
+    }
+}
+star(8)
+
+// *
+// **
+// ***
+// ****    n=4
+function star(n) {
+    for (let i = 0; i < n; i++){
+        let row = "";
+        for (let j = 0; j <= i; j++){
+            row += '* ';
+        }
+        console.log(row);
+    }
+}
+star(4)
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+function star(n) {
+    for (let i = 1; i <= n; i++){
+        let row = "";
+        for (let j = 1; j <= i; j++){
+            row += `${j} `;
+        }
+        console.log(row);
+    }
+}
+star(5)
